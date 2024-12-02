105bd5c9afcb987a3c3b955b35ce008ab6b8a3d6
Noe <dionico_alegarbes@ring-group.jp>
2024-12-02 09:42:09 +0800

    added japanese date picker

---
+++ JapDatePicker.vue
@@ -0,0 +1,99 @@
+<template>
+    <v-menu :close-on-content-click="false">
+        <template v-slot:activator="{ props }">
+            <v-text-field
+                variant="outlined"
+                density="compact"
+                v-bind="props"
+                readonly
+                clearable
+                v-model="formattedDate"
+                @click:clear="clearDate"
+                placeholder="Select Date"
+                append-inner-icon="mdi-calendar"
+            ></v-text-field>
+        </template>
+        <v-locale-provider
+            :locale="DATE_FORMAT_JA_JP"
+            :fallback-locale="DATE_FORMAT_JA"
+        >
+            <v-date-picker
+                v-model="selectedDate"
+                hide-header
+                :view-mode="viewMode"
+                elevation="4"
+                @update:year="onYearSelected"
+                @update:month="onMonthSelected"
+            ></v-date-picker>
+        </v-locale-provider>
+    </v-menu>
+</template>
+
+<script setup>
+    /**
+     * @prop {string} mode = use to change view-mode of date-picker | accepted values: 'month', 'months', 'year'
+     *
+     * Refer to /test page for demo use
+     *
+     */
+    import { ref, computed, watch } from "vue";
+    import { DATE_FORMAT_JA, DATE_FORMAT_JA_JP, YEAR_MONTH_DAY_DASH } from "~/constants/datetime";
+
+    const props = defineProps(["mode"]);
+
+    const selectedDate = ref(null);
+    const viewMode = ref(props.mode || "month");
+    const emit = defineEmits(["selectedDate"]);
+
+    const formattedDate = computed(() => {
+        if (selectedDate.value) {
+            const commonDate = new Intl.DateTimeFormat("ja-JP", {
+                year: "numeric",
+                month: "2-digit",
+                day: "2-digit",
+            }).format(new Date(selectedDate.value));
+
+            const displayDate = formatDisplay(commonDate);
+            updateDateResults(commonDate);
+            return displayDate;
+        }
+        return "";
+    });
+
+    const updateDateResults = (commonDate) => {
+        const formattedDate = FormatDate(commonDate, YEAR_MONTH_DAY_DASH);
+        emit("selectedDate", !isValidDate(formattedDate) ? null : formattedDate);
+    };
+
+    const formatDisplay = (date) => {
+        const dateObj = new Date(date);
+        const japaneseDateShort = dateObj
+            .toLocaleDateString(DATE_FORMAT_JA_JP, { era: "long" })
+            .replace(/(\d+)\/(\d+)\/(\d+)/, "$1年");
+        return `${japaneseDateShort}(${date})`;
+    };
+
+    const clearDate = () => {
+        selectedDate.value = null;
+        updateDateResults(null, null);
+        viewMode.value = "year";
+    };
+
+    // Functions to handle view mode change on selection
+    const onYearSelected = () => {
+        viewMode.value = "months";
+    };
+
+    const onMonthSelected = () => {
+        viewMode.value = "month";
+    };
+
+    watch(selectedDate, (newValue) => {
+        if (newValue == null) {
+            updateDateResults(null, null);
+            if(!props.mode) {
+                viewMode.value = "month";
+            }
+        }
+    });
+</script>
---
+++ test.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-container class="ma-0 d-flex flex-column justify-start w-100">
+        
+        <!-- sample use for japDatepicker -->
+        <v-row class="pa-3 w-75">
+            <v-col class="mx-2 pa-0" cols="4">
+                <p>Default</p>
+                <JapDatePicker 
+                    @selected-date="dateValue1 = $event"
+                />
+                <p>Return Value: {{ dateValue1 }}</p>
+            </v-col>
+            <v-col class="mx-2 pa-0" cols="4">
+                <p>Specific Mode</p>
+                <JapDatePicker 
+                    @selected-date="dateValue2 = $event"
+                    mode="year"
+                />
+                <p>Return Value: {{ dateValue2 }}</p>
+            </v-col>
+        </v-row>
+
+    </v-container>
+</template>
+
+<script setup>
+    import { ref } from "vue";
+
+    const dateValue1 = ref(null)
+    const dateValue2 = ref(null)
+
+</script>
+
+<style scoped></style>
